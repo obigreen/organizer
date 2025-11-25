3f4ea99677d6a1df5061eaaad032985ce7079b71
Sergey Budyakin <youshtuchka@gmail.com>
2025-11-25 22:24:04 +0300

    Wrote function shuffle, filterSimilar, preparePools and added symbols for generate and for filters

--- tools/password-generator/script.js
+++ tools/password-generator/script.js
@@ -25,6 +25,64 @@
     // ===============
     function shuffle(array) {
 
+        for (let i = array.length - 1; i > 0; i--) {
+
+            const j = randomInit(i + 1);
+
+            const temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+
+        }
+
+        return array;
+    }
+
+
+    const similar = new Set(["l", "I", "1", "O", "0"]);
+    const upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+    const lower = "abcdefghijklmnopqrstuvwxyz".split("");
+    const digits = "0123456789".split("");
+    const symbols = "!@#$%^&*()-_=+[]{};:,.?/~\\\\|<>".split("");
+
+
+    function filterSimilar(arr, avoidSimilar) {
+        if (!avoidSimilar) {
+            return arr;
+        }
+
+        return arr.filter(item => !similar.has(item));
+    }
+
+
+    function preparePools(options) {
+
+        const upperPool = filterSimilar(upper, options.avoidSimilar);
+        const lowerPool = filterSimilar(lower, options.avoidSimilar);
+        const digitPool = filterSimilar(digits, options.avoidSimilar);
+        const symbolPool = options.includeSymbols ? filterSimilar(symbols, options.avoidSimilar) : [];
+
+        let fullPool = [
+            ...upperPool, ...lowerPool, ...digitPool, ...symbolPool
+        ]
+
+        if (fullPool.length === 0) {
+            fullPool = [...lower];
+        }
+
+        const must = [];
+
+
+        if (options.requireEach) {
+            if (upperPool.length) must.push(upperPool[randomInit(upperPool.length)]);
+            if (lowerPool.length) must.push(lowerPool[randomInit(lowerPool.length)]);
+            if (digitPool.length) must.push(digitPool[randomInit(digitPool.length)]);
+            if (symbolPool.length) must.push(symbolPool[randomInit(symbolPool.length)]);
+        }
+
+        return {
+            upperPool, lowerPool, digitPool, symbolPool, fullPool, must
+        }
 
     }
 
